faec86253c07ebf6a3bea91d792035864197ac25
0Carvalh0 <joctor.carvalho2004@gmail.com>
2023-12-12 23:42:51 -0300

    Adição do calculo de °K (Kelvin)

--- Desafios/d006/script/script.js
+++ Desafios/d006/script/script.js
@@ -1,7 +1,9 @@
-let cel = document.querySelector('button#botao').value
-let fah = (cel * 1.8) + 32
 let res = document.querySelector('div#res')
 function converter() {
-    window.prompt(`Digite uma temperatura em °C (Celsius)`)
-    res.innerHTML = `${fah}`
+    let cel = window.prompt(`Digite uma temperatura em °C (Celsius)`)
+    let fah = cel * 1.8 + 32
+    let kel = cel + 273.15
+    res.innerHTML = `<h2>A temperatura de ${cel}°C, corresponde a...</h2>`
+    res.innerHTML += `<p>${kel}°K (Kelvin)</p>`
+    res.innerHTML += `<p>${fah}°F (Fahrenheit)</p>`
 }
