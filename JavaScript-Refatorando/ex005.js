5639f0121b5b809863bf5dd37e8eaaee7339aae0
João Victor Carvalho <149029065+0Carvalh0@users.noreply.github.com>
2024-01-16 20:42:18 -0300

    Exercício 5

---
+++ JavaScript-Refatorando/ex005.js
@@ -0,0 +1,7 @@
+let n1 = 5
+let n2 = 8
+let n3 = 10
+let media = (n1 + n2 + n3) / 3
+let apresentacao = 'A média de ' + n1 + ' e ' + n2 + ' e ' + n3 + ', é igual á ' + media.toFixed(2)
+
+console.log(apresentacao)
