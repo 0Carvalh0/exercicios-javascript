f784d00b3e23e7cd06a4ea6b3168a5039b5a513f
João Victor Carvalho <149029065+0Carvalh0@users.noreply.github.com>
2024-01-16 20:53:39 -0300

    Exercício 8

---
+++ JavaScript-Refatorando/ex008.js
@@ -0,0 +1,7 @@
+let livro = {
+    titulo: 'Harry Potter e o Enigma do Principe',
+    autor: 'J.K. Rolling',
+    numerodePaginas: 512
+}
+
+console.log(`Eu gosto muito do livro ${livro.titulo.toUpperCase()} do/da autor(a) ${livro.autor}, mas ele é muito grande, possui ${livro.numerodePaginas} páginas!`)
