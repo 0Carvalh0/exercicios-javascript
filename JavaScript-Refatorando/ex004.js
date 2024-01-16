923bb112495d52bbb0a87ba899b642d3febb159b
João Victor Carvalho <149029065+0Carvalh0@users.noreply.github.com>
2024-01-16 20:36:51 -0300

    Exercicio 4

---
+++ JavaScript-Refatorando/ex004.js
@@ -0,0 +1,7 @@
+let nome = 'João Victor'
+let sobrenome = "Carvalho"
+
+let nomeMai = nome.toUpperCase()
+let sobrenomeMai = sobrenome.toUpperCase()
+
+console.log(`Seu nome é ${nomeMai} e seu sobrenome é ${sobrenomeMai}.`)
