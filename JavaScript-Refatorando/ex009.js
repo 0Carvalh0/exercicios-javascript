be47ff6d8ec66ea088f394839b0992d19f4a2e96
João Victor Carvalho <149029065+0Carvalh0@users.noreply.github.com>
2024-01-17 00:58:50 -0300

    Exercício 9

---
+++ JavaScript-Refatorando/ex009.js
@@ -0,0 +1,16 @@
+let a = 20
+let b = 13
+
+let soma = a + b
+let subtracao = a - b
+let multiplicacao = a * b
+let divisao = a / b
+let resto = a % b
+
+a++
+a += 10
+b -= 3
+
+a *= divisao
+
+console.log(a.toFixed(2))
