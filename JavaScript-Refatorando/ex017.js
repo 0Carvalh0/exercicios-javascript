2a4d0275b331687fd02419b361d399d3adfadb27
João Victor Carvalho <149029065+0Carvalh0@users.noreply.github.com>
2024-01-18 22:18:42 -0300

    Exercício 17

---
+++ JavaScript-Refatorando/ex017.js
@@ -0,0 +1,9 @@
+let num = 0;
+
+if (num == 0) {
+  console.log(`O número é zero!`);
+} else if (num >= 0) {
+  console.log(`O número ${num} é POSITIVO!`);
+} else {
+  console.log(`O número ${num} é NEGATIVO!`);
+}
