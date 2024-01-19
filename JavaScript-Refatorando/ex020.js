d64a569e6e6b9e3ed211ccc0cd83f0a3d8186c0c
João Victor Carvalho <149029065+0Carvalh0@users.noreply.github.com>
2024-01-18 22:28:43 -0300

    Exercício 20

---
+++ JavaScript-Refatorando/ex020.js
@@ -0,0 +1,7 @@
+let valor = 20
+
+if (valor > 15 && valor < 35) {
+    console.log(`O valor ${valor} é maior que 15 e menor do que 35!`)
+} else {
+    console.log(`O valor ${valor} não é maior que 15 e menor do que 35!`)
+}
