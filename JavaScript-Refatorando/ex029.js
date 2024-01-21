f913f4aa76bed364bc7be6f5dfecdc2d1a54f7c3
João Victor Carvalho <149029065+0Carvalh0@users.noreply.github.com>
2024-01-20 21:25:25 -0300

    Exercício 29

---
+++ JavaScript-Refatorando/ex029.js
@@ -0,0 +1,10 @@
+let num = "5"
+
+switch (num) {
+    case 5:
+        console.log(`Você digitou o número 5`)
+        break
+    case "5":
+        console.log(`Você escreveu a palavra 5`)
+        break
+}
