2a247e7c6151bde4819598db1d048e8a4617aab8
João Victor Carvalho <149029065+0Carvalh0@users.noreply.github.com>
2024-01-20 21:17:49 -0300

    Exercício 27

---
+++ JavaScript-Refatorando/ex027.js
@@ -0,0 +1,13 @@
+let fruta = "Mamão"
+
+switch (fruta) {
+    case "Mamão":
+        console.log(`O ${fruta} custa R$8,99`)
+        break
+    case "Maça":
+        console.log(`O ${fruta} custa R$5,00`)
+        break
+    case "Banana":
+        console.log(`A ${fruta} custa R$15,00`)
+        break
+}
